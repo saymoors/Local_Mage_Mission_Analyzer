0ec5ec4b5ba618db6597decc91eb5c38d5b98af7
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-03-09 21:30:15 +0300

    Обновление MainMenu
    Добавлен рабочий GUI с выбором файла.

--- src/main/java/GUI/MainMenu.java
+++ src/main/java/GUI/MainMenu.java
@@ -1,11 +1,60 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
-public class MainMenu {
+public class MainMenu extends JFrame {
     JPanel panel;
+    JLabel label;
+    JFileChooser chooser;
+    JButton button;
+    File file;
 
     public MainMenu() {
-        
+        panel = new JPanel();
+        label = new JLabel("Выберите миссию:");
+        button = new JButton("Открыть магический поисковик");
+        chooser = new JFileChooser();
+
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        panel.setPreferredSize(new Dimension(260, 90));
+        button.setPreferredSize(new Dimension(240, 40));
+
+        button.addActionListener(e -> {
+            try {
+                int result = chooser.showOpenDialog(this);
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    file = chooser.getSelectedFile();
+                    JOptionPane.showMessageDialog(
+                            this,
+                            "Вы выбрали: " + file,
+                            "Уведомление",
+                            JOptionPane.INFORMATION_MESSAGE
+                    );
+                } else {
+                    throw new Exception("Вы не выбрали миссию!");
+                }
+            } catch (Exception exception) {
+                JOptionPane.showMessageDialog(
+                        this,
+                        exception.getMessage(),
+                        "Ошибка",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            }
+        });
+
+        panel.add(label);
+        panel.add(button);
+        getContentPane().add(panel);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setName("Локальный анализатор миссий");
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
     }
 }
