36cd5bfcecb31c83bf66b3684b37ab3dd37c52dd
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-03-10 23:22:55 +0300

    Создание парсера txt!!!
    Очень просто и без особых проверок. Надежда на правильность формата

--- src/main/java/Parsers/ParserTXT.java
+++ src/main/java/Parsers/ParserTXT.java
@@ -1,4 +1,77 @@
 package Parsers;
 
-public class ParserTXT {
+import Entities.Curse;
+import Entities.Mission;
+import Entities.Sorcerer;
+import Entities.Technique;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParserTXT implements IParser {
+    @Override
+    public Mission parse(String file) throws Exception {
+        List<String> data = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    data.add(line);
+                }
+            }
+            reader.close();
+        } catch (IOException exception) {
+            throw new Exception("Не удалось прочитать TXT-руну!");
+        }
+
+        int i = 0;
+        Mission mission = new Mission();
+        mission.setMissionId(cuttingOf(data.get(i++)));
+        mission.setDate(cuttingOf(data.get(i++)));
+        mission.setLocation(cuttingOf(data.get(i++)));
+        mission.setOutcome(cuttingOf(data.get(i++)));
+        mission.setDamageCost(Integer.parseInt(cuttingOf(data.get(i++))));
+
+        Curse curse = new Curse();
+        curse.setName(cuttingOf(data.get(i++)));
+        curse.setThreatLevel(cuttingOf(data.get(i++)));
+        mission.setCurse(curse);
+
+        List<Sorcerer> sorcerers = new ArrayList<>();
+        while(i < data.size() && data.get(i).startsWith("sorcerer[")) {
+            Sorcerer sorcerer = new Sorcerer();
+            sorcerer.setName(cuttingOf(data.get(i++)));
+            sorcerer.setRank(cuttingOf(data.get(i++)));
+            sorcerers.add(sorcerer);
+        }
+        mission.setSorcerers(sorcerers);
+
+        List<Technique> techniques = new ArrayList<>();
+        while(i < data.size() && data.get(i).startsWith("technique[")) {
+            Technique technique = new Technique();
+            technique.setName(cuttingOf(data.get(i++)));
+            technique.setType(cuttingOf(data.get(i++)));
+            technique.setOwner(cuttingOf(data.get(i++)));
+            technique.setDamage(Integer.parseInt(cuttingOf(data.get(i++))));
+            techniques.add(technique);
+        }
+        mission.setTechniques(techniques);
+
+        if (i < data.size() && data.get(i).startsWith("note:")) {
+            mission.setComment(cuttingOf(data.get(i)));
+        }
+
+        return mission;
+    }
+
+    private String cuttingOf(String line) {
+        int colonIndex = line.indexOf(':');
+        return line.substring(colonIndex + 2);
+    }
 }
