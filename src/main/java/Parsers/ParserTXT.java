e354da58a1d626975564ec50ed404d26ef301ce4
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-03-11 17:50:11 +0300

    Обработка исключений

--- src/main/java/Parsers/ParserTXT.java
+++ src/main/java/Parsers/ParserTXT.java
@@ -16,7 +16,9 @@ import java.util.List;
 public class ParserTXT implements IParser {
     @Override
     public Mission parse(String file) throws Exception {
+        Mission mission = new Mission();
         List<String> data = new ArrayList<>();
+
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
             String line;
@@ -26,45 +28,45 @@ public class ParserTXT implements IParser {
                 }
             }
             reader.close();
-        } catch (IOException exception) {
+            
+            int i = 0;
+
+            mission.setMissionId(cuttingOf(data.get(i++)));
+            mission.setDate(cuttingOf(data.get(i++)));
+            mission.setLocation(cuttingOf(data.get(i++)));
+            mission.setOutcome(cuttingOf(data.get(i++)));
+            mission.setDamageCost(Integer.parseInt(cuttingOf(data.get(i++))));
+
+            Curse curse = new Curse();
+            curse.setName(cuttingOf(data.get(i++)));
+            curse.setThreatLevel(cuttingOf(data.get(i++)));
+            mission.setCurse(curse);
+
+            List<Sorcerer> sorcerers = new ArrayList<>();
+            while (i < data.size() && data.get(i).startsWith("sorcerer[")) {
+                Sorcerer sorcerer = new Sorcerer();
+                sorcerer.setName(cuttingOf(data.get(i++)));
+                sorcerer.setRank(cuttingOf(data.get(i++)));
+                sorcerers.add(sorcerer);
+            }
+            mission.setSorcerers(sorcerers);
+
+            List<Technique> techniques = new ArrayList<>();
+            while (i < data.size() && data.get(i).startsWith("technique[")) {
+                Technique technique = new Technique();
+                technique.setName(cuttingOf(data.get(i++)));
+                technique.setType(cuttingOf(data.get(i++)));
+                technique.setOwner(cuttingOf(data.get(i++)));
+                technique.setDamage(Integer.parseInt(cuttingOf(data.get(i++))));
+                techniques.add(technique);
+            }
+            mission.setTechniques(techniques);
+
+            if (i < data.size() && data.get(i).startsWith("note:")) {
+                mission.setComment(cuttingOf(data.get(i)));
+            }
+        } catch(Exception exception) {
             throw new Exception("Не удалось прочитать TXT-руну!");
-        }
-
-        int i = 0;
-        Mission mission = new Mission();
-        mission.setMissionId(cuttingOf(data.get(i++)));
-        mission.setDate(cuttingOf(data.get(i++)));
-        mission.setLocation(cuttingOf(data.get(i++)));
-        mission.setOutcome(cuttingOf(data.get(i++)));
-        mission.setDamageCost(Integer.parseInt(cuttingOf(data.get(i++))));
-
-        Curse curse = new Curse();
-        curse.setName(cuttingOf(data.get(i++)));
-        curse.setThreatLevel(cuttingOf(data.get(i++)));
-        mission.setCurse(curse);
-
-        List<Sorcerer> sorcerers = new ArrayList<>();
-        while(i < data.size() && data.get(i).startsWith("sorcerer[")) {
-            Sorcerer sorcerer = new Sorcerer();
-            sorcerer.setName(cuttingOf(data.get(i++)));
-            sorcerer.setRank(cuttingOf(data.get(i++)));
-            sorcerers.add(sorcerer);
-        }
-        mission.setSorcerers(sorcerers);
-
-        List<Technique> techniques = new ArrayList<>();
-        while(i < data.size() && data.get(i).startsWith("technique[")) {
-            Technique technique = new Technique();
-            technique.setName(cuttingOf(data.get(i++)));
-            technique.setType(cuttingOf(data.get(i++)));
-            technique.setOwner(cuttingOf(data.get(i++)));
-            technique.setDamage(Integer.parseInt(cuttingOf(data.get(i++))));
-            techniques.add(technique);
-        }
-        mission.setTechniques(techniques);
-
-        if (i < data.size() && data.get(i).startsWith("note:")) {
-            mission.setComment(cuttingOf(data.get(i)));
         }
 
         return mission;
