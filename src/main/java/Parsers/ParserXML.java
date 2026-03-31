18ed9d951ccdaead447d44b776965d3fb2aa21d5
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-03-31 22:37:40 +0300

    Обновление парсера XML
    Меняется логика - создается объект через фабрику, только потом данные накладываются на него через xmlMapper. Плюс создание объекта происходит через фабрику, а не напрямую!

--- src/main/java/Parsers/ParserXML.java
+++ src/main/java/Parsers/ParserXML.java
@@ -1,6 +1,7 @@
 package Parsers;
 
 import Entities.Mission;
+import Factories.MissionFactory;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import java.io.File;
@@ -8,15 +9,18 @@ import java.io.IOException;
 
 public class ParserXML implements IParser {
     private final XmlMapper xmlMapper;
+    private final MissionFactory missionFactory;
 
-    public ParserXML() {
+    public ParserXML(MissionFactory missionFactory) {
         xmlMapper = new XmlMapper();
+        this.missionFactory = missionFactory;
     }
 
     @Override
     public Mission parse(String file) throws Exception {
         try {
-            Mission mission = xmlMapper.readValue(new File(file), Mission.class);
+            Mission mission = missionFactory.createMission();
+            xmlMapper.readerForUpdating(mission).readValue(new File(file));
             mission.linkEntities();
             return mission;
         } catch (IOException exception) {
