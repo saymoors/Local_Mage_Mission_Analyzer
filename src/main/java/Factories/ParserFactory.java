0cacc33a1a63510710b9d9ad1c02d418d5acb406
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-03-31 22:49:15 +0300

    Обновление ParserFactory
    Передача фабрики миссий для создания парсера!

--- src/main/java/Factories/ParserFactory.java
+++ src/main/java/Factories/ParserFactory.java
@@ -6,11 +6,17 @@ import Parsers.ParserTXT;
 import Parsers.ParserXML;
 
 public class ParserFactory {
+    private final MissionFactory missionFactory;
+
+    public ParserFactory(MissionFactory missionFactory) {
+        this.missionFactory = missionFactory;
+    }
+
     public IParser createParser(String extension) throws Exception {
         return switch (extension.toLowerCase()) {
-            case "json" -> new ParserJSON();
-            case "xml" -> new ParserXML();
-            case "txt" -> new ParserTXT();
+            case "json" -> new ParserJSON(missionFactory);
+            case "xml" -> new ParserXML(missionFactory);
+            case "txt" -> new ParserTXT(missionFactory);
             default -> throw new Exception("Вы выбрали иную руну!");
         };
     }
