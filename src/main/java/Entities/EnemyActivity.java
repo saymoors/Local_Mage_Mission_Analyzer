1483489cc9fa154eb523f8e89c45c4567fc48f5f
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-01 11:12:15 +0300

    Добавление нового класса из XML

---
+++ src/main/java/Entities/EnemyActivity.java
@@ -0,0 +1,65 @@
+package Entities;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+import java.util.List;
+
+public class EnemyActivity {
+    private String behaviorType;
+    private String targetPriority;
+    private String mobility;
+    private String escalationRisk;
+    private List<String> attackPatterns;
+    private List<String> countermeasuresUsed;
+
+    public EnemyActivity() { }
+
+    public String getBehaviorType() {
+        return behaviorType;
+    }
+
+    public void setBehaviorType(String behaviorType) {
+        this.behaviorType = behaviorType;
+    }
+
+    public String getTargetPriority() {
+        return targetPriority;
+    }
+
+    public void setTargetPriority(String targetPriority) {
+        this.targetPriority = targetPriority;
+    }
+
+    public String getMobility() {
+        return mobility;
+    }
+
+    public void setMobility(String mobility) {
+        this.mobility = mobility;
+    }
+
+    public String getEscalationRisk() {
+        return escalationRisk;
+    }
+
+    public void setEscalationRisk(String escalationRisk) {
+        this.escalationRisk = escalationRisk;
+    }
+
+    public List<String> getAttackPatterns() {
+        return attackPatterns;
+    }
+
+    public void setAttackPatterns(List<String> attackPatterns) {
+        this.attackPatterns = attackPatterns;
+    }
+
+    public List<String> getCountermeasuresUsed() {
+        return countermeasuresUsed;
+    }
+
+    public void setCountermeasuresUsed(List<String> countermeasuresUsed) {
+        this.countermeasuresUsed = countermeasuresUsed;
+    }
+}
