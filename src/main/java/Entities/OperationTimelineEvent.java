a752db7433d9fd3b5db0fb3372f4689138932698
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-01 16:31:13 +0300

    Добавление новой сущности

---
+++ src/main/java/Entities/OperationTimelineEvent.java
@@ -0,0 +1,33 @@
+package Entities;
+
+public class OperationTimelineEvent {
+    private String timestamp;
+    private String type;
+    private String description;
+
+    public OperationTimelineEvent() { }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
