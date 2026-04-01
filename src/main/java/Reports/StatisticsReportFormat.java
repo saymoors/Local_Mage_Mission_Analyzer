8f5226492779d7f9169092bd0b7fb012359b16ca
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-02 00:41:39 +0300

    Небольшое сокращение объема

--- src/main/java/Reports/StatisticsReportFormat.java
+++ src/main/java/Reports/StatisticsReportFormat.java
@@ -1,11 +1,9 @@
 package Reports;
 
 import Entities.Mission;
-import Entities.Sorcerer;
 import Entities.Technique;
 
 import javax.swing.*;
-import java.util.List;
 
 public class StatisticsReportFormat implements IReportFormat {
     @Override
@@ -13,28 +11,25 @@ public class StatisticsReportFormat implements IReportFormat {
         panel.add(new JLabel("missionId:"));
         panel.add(new JLabel(mission.getMissionId()));
 
-        List<Sorcerer> sorcerers = mission.getSorcerers();
-        int sorcerersCount = sorcerers.size();
+        int sorcerersCount = mission.getSorcerers().size();
         panel.add(new JLabel("sorcerers.count:"));
         panel.add(new JLabel(String.valueOf(sorcerersCount)));
 
-        List<Technique> techniques = mission.getTechniques();
-        int techniquesCount = techniques.size();
+        int techniquesCount = mission.getTechniques().size();
         panel.add(new JLabel("techniques.count:"));
         panel.add(new JLabel(String.valueOf(techniquesCount)));
 
         int totalDamage = 0;
-        for (Technique technique : techniques) {
+        for (Technique technique : mission.getTechniques()) {
             totalDamage += technique.getDamage();
         }
 
         panel.add(new JLabel("techniques.totalDamage:"));
         panel.add(new JLabel(String.valueOf(totalDamage)));
 
-        if(techniquesCount != 0) {
-            String average = String.valueOf(totalDamage / techniquesCount);
+        if (techniquesCount != 0) {
             panel.add(new JLabel("techniques.avgDamage:"));
-            panel.add(new JLabel(average));
+            panel.add(new JLabel(String.valueOf(totalDamage / techniquesCount)));
         }
     }
 }
