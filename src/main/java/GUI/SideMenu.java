5556229ebc0d596906fbcb555200058e961c5a88
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-02 00:58:01 +0300

    Внесение правок

--- src/main/java/GUI/SideMenu.java
+++ src/main/java/GUI/SideMenu.java
@@ -1,7 +1,6 @@
 package GUI;
 
 import Entities.Mission;
-import Reports.DetailedReportFormat;
 import Reports.IReportActions;
 import Reports.IReportFormat;
 
@@ -9,20 +8,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class SideMenu extends JDialog {
-
-    public SideMenu(Mission mission) {
-        this(mission, new DetailedReportFormat());
-    }
-
     public SideMenu(Mission mission, IReportFormat format) {
-        IReportActions dialogActions = new TableDialogActions(this);
+        IReportActions reportActions = new TableReportActions(this);
         JPanel panel = new JPanel();
 
         panel.setLayout(new GridLayout(0, 2, 10, 10));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        format.render(mission, panel, dialogActions);
-
+        format.render(mission, panel, reportActions);
         getContentPane().add(panel);
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
