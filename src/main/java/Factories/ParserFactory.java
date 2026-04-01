aa3e29a193cb360ad97481fbb4865f98edc3b738
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-02 00:59:45 +0300

    Упрощение ParserFactory

--- src/main/java/Factories/ParserFactory.java
+++ src/main/java/Factories/ParserFactory.java
@@ -4,21 +4,13 @@ import Parsers.IParser;
 import Parsers.ParserJSON;
 import Parsers.ParserTXT;
 import Parsers.ParserXML;
-import Parsers.ParserYAML;
 
 public class ParserFactory {
-    private final MissionFactory missionFactory;
-
-    public ParserFactory(MissionFactory missionFactory) {
-        this.missionFactory = missionFactory;
-    }
-
     public IParser createParser(String extension) throws Exception {
         return switch (extension.toLowerCase()) {
-            case "json" -> new ParserJSON(missionFactory);
-            case "xml" -> new ParserXML(missionFactory);
-            case "txt" -> new ParserTXT(missionFactory);
-            case "yaml" -> new ParserYAML(missionFactory);
+            case "json" -> new ParserJSON();
+            case "xml" -> new ParserXML();
+            case "txt" -> new ParserTXT();
             default -> throw new Exception("Вы выбрали иную руну!");
         };
     }
