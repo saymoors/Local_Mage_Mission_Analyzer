3ae8717c81f15f8d8ee67d0bbdf22a00253bcfcf
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-01 16:31:05 +0300

    Добавление новой сущности

---
+++ src/main/java/Entities/CivilianImpact.java
@@ -0,0 +1,42 @@
+package Entities;
+
+public class CivilianImpact {
+    private int evacuated;
+    private int injured;
+    private int missing;
+    private String publicExposureRisk;
+
+    public CivilianImpact() { }
+
+    public int getEvacuated() {
+        return evacuated;
+    }
+
+    public void setEvacuated(int evacuated) {
+        this.evacuated = evacuated;
+    }
+
+    public int getInjured() {
+        return injured;
+    }
+
+    public void setInjured(int injured) {
+        this.injured = injured;
+    }
+
+    public int getMissing() {
+        return missing;
+    }
+
+    public void setMissing(int missing) {
+        this.missing = missing;
+    }
+
+    public String getPublicExposureRisk() {
+        return publicExposureRisk;
+    }
+
+    public void setPublicExposureRisk(String publicExposureRisk) {
+        this.publicExposureRisk = publicExposureRisk;
+    }
+}
