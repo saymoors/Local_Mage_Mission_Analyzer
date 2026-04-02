45886185fa27a933e9d2b45f1587d8e00c3a6005
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-02 12:56:11 +0300

    Добавление новых парсеров

--- src/main/java/Factories/ParserFactory.java
+++ src/main/java/Factories/ParserFactory.java
@@ -1,16 +1,20 @@
 package Factories;
 
 import Parsers.IParser;
+import Parsers.ParserFWE;
 import Parsers.ParserJSON;
 import Parsers.ParserTXT;
 import Parsers.ParserXML;
+import Parsers.ParserYAML;
 
 public class ParserFactory {
     public IParser createParser(String extension) throws Exception {
         return switch (extension.toLowerCase()) {
             case "json" -> new ParserJSON();
             case "xml" -> new ParserXML();
+            case "yaml" -> new ParserYAML();
             case "txt" -> new ParserTXT();
+            case "" -> new ParserFWE();
             default -> throw new Exception("Вы выбрали иную руну!");
         };
     }
