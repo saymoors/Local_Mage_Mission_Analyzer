ff8c10a5a5b5cc74035c1a96fb4af8064f031a60
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-02 11:42:27 +0300

    Создание YAML-парсера

---
+++ src/main/java/Parsers/ParserYAML.java
@@ -0,0 +1,28 @@
+package Parsers;
+
+import Builders.MissionBuilder;
+import Entities.Mission;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ParserYAML implements IParser {
+    private final YAMLMapper yamlMapper;
+
+    public ParserYAML() {
+        yamlMapper = new YAMLMapper();
+    }
+
+    @Override
+    public Mission parse(String file) throws Exception {
+        try {
+            Mission readMission = yamlMapper.readValue(new File(file), Mission.class);
+            Mission createdMission = MissionBuilder.fromMission(readMission).build();
+            createdMission.linkEntities();
+            return createdMission;
+        } catch (IOException exception) {
+            throw new Exception("Не удалось прочитать YAML-руну!");
+        }
+    }
+}
