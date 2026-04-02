584eb8766968da5fe33b7800549f3e6bfb652b60
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-02 13:31:36 +0300

    Дополнительная проверка

--- src/main/java/Builders/MissionBuilder.java
+++ src/main/java/Builders/MissionBuilder.java
@@ -214,6 +214,42 @@ public class MissionBuilder {
     }
 
     public Mission build() {
+        if (mission.getSorcerers() == null) {
+            mission.setSorcerers(new ArrayList<>());
+        }
+
+        if (mission.getTechniques() == null) {
+            mission.setTechniques(new ArrayList<>());
+        }
+
+        if (mission.getOperationTimeline() == null) {
+            mission.setOperationTimeline(new ArrayList<>());
+        }
+
+        if (mission.getOperationTags() == null) {
+            mission.setOperationTags(new ArrayList<>());
+        }
+
+        if (mission.getSupportUnits() == null) {
+            mission.setSupportUnits(new ArrayList<>());
+        }
+
+        if (mission.getRecommendations() == null) {
+            mission.setRecommendations(new ArrayList<>());
+        }
+
+        if (mission.getArtifactsRecovered() == null) {
+            mission.setArtifactsRecovered(new ArrayList<>());
+        }
+
+        if (mission.getEvacuationZones() == null) {
+            mission.setEvacuationZones(new ArrayList<>());
+        }
+
+        if (mission.getStatusEffects() == null) {
+            mission.setStatusEffects(new ArrayList<>());
+        }
+
         return mission;
     }
 }
