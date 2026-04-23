f4217d7c4aae2116a5a9a4312f2545b89173fc97
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-23 22:31:27 +0300

    Создание запускатора

---
+++ src/main/java/Archive/MissionArchiveApplication.java
@@ -0,0 +1,11 @@
+package Archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionArchiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MissionArchiveApplication.class, args);
+    }
+}
