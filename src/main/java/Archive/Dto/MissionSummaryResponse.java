595b43be3f4d19c50c8d5d9f341ad7477286ecf8
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-23 23:26:41 +0300

    Добавление метода формирования краткого вида миссии

--- src/main/java/Archive/Dto/MissionSummaryResponse.java
+++ src/main/java/Archive/Dto/MissionSummaryResponse.java
@@ -1,9 +1,19 @@
 package Archive.Dto;
 
+import Entities.Mission;
+
 public record MissionSummaryResponse(
         String missionId,
         String date,
         String location,
         String outcome
 ) {
+    public static MissionSummaryResponse fromMission(Mission mission) {
+        return new MissionSummaryResponse(
+                mission.getMissionId(),
+                mission.getDate(),
+                mission.getLocation(),
+                mission.getOutcome().name()
+        );
+    }
 }
