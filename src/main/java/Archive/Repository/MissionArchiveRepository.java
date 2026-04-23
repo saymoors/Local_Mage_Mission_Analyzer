1e018103278067b3fb1986bfa24405fa3fadf821
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-04-23 23:26:00 +0300

    Переписание контракта - findByMissionId возвращает null

--- src/main/java/Archive/Repository/MissionArchiveRepository.java
+++ src/main/java/Archive/Repository/MissionArchiveRepository.java
@@ -3,12 +3,11 @@ package Archive.Repository;
 import Entities.Mission;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MissionArchiveRepository {
     Mission save(Mission mission);
 
-    Optional<Mission> findByMissionId(String missionId);
+    Mission findByMissionId(String missionId);
 
     List<Mission> findAll();
 }
